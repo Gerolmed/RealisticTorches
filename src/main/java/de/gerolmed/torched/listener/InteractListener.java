137abe62b76bbb7aed22a19eeb7328d3d4e44185
Gerolmed <werwolf420@yahoo.de>
2019-02-05 16:39:26 +0100

    Rechargeable torches

--- src/main/java/de/gerolmed/torched/listener/InteractListener.java
+++ src/main/java/de/gerolmed/torched/listener/InteractListener.java
@@ -2,11 +2,13 @@ package de.gerolmed.torched.listener;
 
 import de.gerolmed.torched.Main;
 import de.gerolmed.torched.utils.BasicEvent;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class InteractListener extends BasicEvent {
 
@@ -17,6 +19,12 @@ public class InteractListener extends BasicEvent {
     @EventHandler(priority = EventPriority.MONITOR)
     public void interact(PlayerInteractEvent event) {
         if(event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+
+            ItemStack itemStack = event.getPlayer().getItemInHand();
+
+            if(itemStack == null || (itemStack.getType() != Material.FLINT_AND_STEEL && itemStack.getType() != Material.FIREBALL))
+                return;
+
             Block block = event.getClickedBlock();
             plugin.getBlockManager().addBlock(block, plugin.getDataHolder().getResetTime());
         }
